8c7e36d744580830f0e3b46631c2021d72bfd623
amdreallyfast <amdreallyfast@hotmail.com>
2014-05-12 17:34:41 -0700

    added Direct3D initialization and cleaning
    I've added initialization for Direct3D 11 and a function to clean it up
    after everything is done.
    
    I don't know if I need to use Direct3D 10.  I need d3dx11.h and d3dx10.h
    according to my tutorial, but I don't have those headers (I don't have
    the DirectX SDK explicitly installed either) and it isn't complaining
    yet.

--- VS_2013_CPP_graphics_pad/main.cpp
+++ VS_2013_CPP_graphics_pad/main.cpp
@@ -1,8 +1,70 @@
-// include the basic windows header file
+// include the basic windows header file and an "extensions" header
 #include <Windows.h>
-
-// ??for what??
 #include <windowsx.h>
+
+// for function declarations of Direct3D basic and extended functions
+#include <d3d11.h>
+
+// for macros that Direct3D 11 uses that are based in Direct3D 10
+//#include <d3d10.h>
+
+// include the Direct3D library files
+#pragma comment (lib, "d3d11.lib")
+//#pragma comment (lib, "d3d10.lib")
+
+// the pointer to the interface for the swap chain (that is, the frame buffer)
+IDXGISwapChain *g_swap_chain;
+
+// the pointer to our Direct3D device interface, which is our program's representation of our video adapter and mostly handles video memory
+ID3D11Device *g_dev;
+
+// the pointer to our Direct3d device context, which manages the GPU and the rendering pipeline
+ID3D11DeviceContext *g_dev_context;
+
+// sets up and initializes Direct3D
+void init_d3d(HWND window_handle)
+{
+   // create a struct to hold information about the swap chain
+   DXGI_SWAP_CHAIN_DESC swap_chain_desc;
+
+   // clear out the struct for use
+   ZeroMemory(&swap_chain_desc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+   // fill the swap chain description structure
+   swap_chain_desc.BufferCount = 1;                                  // one back buffer and one front buffer (??is there only one buffer total??) (we could specify more, but we will just start with 1)
+   swap_chain_desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;   // use 32-bit color (8 red bits, 8 green bits, 8 blue bits, 8 alpha bits)
+   swap_chain_desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;    // render the target output to the back buffer of the swap chain (that is, not the currently displayed one)
+   swap_chain_desc.OutputWindow = window_handle;                     // the handle of the window that Direct3D will draw in
+   swap_chain_desc.SampleDesc.Count = 4;                             // how many multisamples Direct3D will perform when blending pixels (that is, anti-aliasing)
+   swap_chain_desc.Windowed = TRUE;                                  // windowed mode (that is, not full-screen mode) because our window is not full screen
+
+   // create a device, device context, and swap chain using the information in the swap chain structure
+   D3D11CreateDeviceAndSwapChain(
+      NULL,                         // adapter; tell Direct3D to use the default graphics adapter
+      D3D_DRIVER_TYPE_HARDWARE,     // driver type; tell Direct3D to use the graphics adapter's hardware for rendering (there are other options, but we will use this one)
+      NULL,                         // handle to software module; the driver type is not "use software", so use NULL
+      NULL,                         // flags; we are using the defaults, so specify NULL
+      NULL,                         // feature level; we don't know anything about my particular video card other than that it is DirectX 11 compliant, so use NULL
+      NULL,                         // number of feature levels; we don't have any feature levels, so use NULL
+      D3D11_SDK_VERSION,            // DirectX SDK version; used for backwards compatability so that Direct3D knows how to adapt varying DirectX 11 versions (our version) to our program
+      &swap_chain_desc,             // pointer to the swap chain description structure
+      &g_swap_chain,                // a pointer to a pointer to the swap chain object
+      &g_dev,                       // a pointer to a pointer to the Direct3D device object
+      NULL,                         // a pointer to a feature level variable; we are not using special features, so use NULL
+      &g_dev_context                // a pointer to a pointer to the device context object
+      );
+}
+
+// closes Direct3D and releases memory
+void clean_d3d(void)
+{
+   // whenever Direct3D is created, it must be closed down
+   // Note: Resources for DirectX are created at the Windows level, and if not released, they will remain running/in memory until the next program restart.
+   g_swap_chain->Release();
+   g_dev->Release();
+   g_dev_context->Release();
+}
+
 
 // this is the main message handler for the program
 LRESULT CALLBACK my_window_proc(HWND handle_window, UINT message, WPARAM w_param, LPARAM l_param)
@@ -67,7 +129,7 @@ int WINAPI WinMain(
    handle_window = CreateWindowEx(
       NULL,                               // do not use any extra style options provided by "Extended Window"
       L"WindowClass1",                    // name of the window class that this window will be an instance of (must match an existing window class name)
-      L"Our First Windowed Program",      // displayed on the window's title bar
+      L"Our First Direct3D Program",      // displayed on the window's title bar
       WS_OVERLAPPEDWINDOW,                // this option specifies several standard features and make a basic window style (check out the #define for more details)
       300,                                // x-position of the window relative to the desktop
       300,                                // y-position of the window relative to the desktop
@@ -80,6 +142,10 @@ int WINAPI WinMain(
 
    // display the window on the screen
    ShowWindow(handle_window, n_show_cmd);
+
+   // set up and initialize Direct3D
+   init_d3d(handle_window);
+
 
    // enter the main loop
 
@@ -115,8 +181,10 @@ int WINAPI WinMain(
       }
    }
 
+   clean_d3d();
+
    // return this part (??which part??) of the WM_QUIT message to Windows
-   return 0;
+   return event_message.wParam;
 }
 
 
